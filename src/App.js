3279b3e599d72d797b081c33a56953cf717163ac
muritador5050 <muritador5050@gmail.com>
2024-10-28 17:06:57 +0100

    install react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
 import "./App.css";
-import Layout from "./components/Layout";
+import Navbar from "./components/Navbar";
+
 export default function App() {
   return (
     <div>
-      <Layout />
+      <Navbar />
     </div>
   );
 }
