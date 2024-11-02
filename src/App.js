7b74cda73776f1e37e205f591c8a00b6ff0269a1
muritador5050 <muritador5050@gmail.com>
2024-11-02 10:24:17 +0100

    setting all routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from "react";
 import "./App.css";
+import Layout from "./components/Layout";
 import Signin from "./components/Signin";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth } from "./config/firebase";
+
 export default function App() {
-  return (
-    <div>
-      {" "}
-      <Signin />
-    </div>
-  );
+  const [user] = useAuthState(auth);
+  return <div>{user ? <Layout /> : <Signin />}</div>;
 }
